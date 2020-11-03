39873a79cdc663a8afa2fb2b8ccc1c4328683514
maxdup <mdupuis@hotmail.ca>
2020-11-03 01:52:22 -0500

    docs - implemented auto documentation for structs

--- docs/ext/autoconstruct.py
+++ docs/ext/autoconstruct.py
@@ -1,21 +1,39 @@
 
+from docutils import nodes
 import sphinx
 import construct
 from sphinx.ext.autodoc import Documenter,  ModuleDocumenter, ModuleLevelDocumenter, ClassLevelDocumenter
 
+from typing import cast
+
+from sphinx import addnodes
 from sphinx.locale import _, __
 from sphinx.util.inspect import safe_getattr
 from sphinx.util.docstrings import prepare_docstring
+from sphinx.util.docfields import DocFieldTransformer
+from sphinx.util.nodes import make_id
+from sphinx.pycode import ModuleAnalyzer, PycodeError
 from sphinx.domains import ObjType
-from sphinx.domains.python import PythonDomain, PyVariable, PyXRefRole
+from sphinx.domains.cpp import CPPDomain
+from sphinx.domains.python import PythonDomain, PyXrefMixin, PyXRefRole, PyObject, PyVariable, PyAttribute, PyClasslike, PyField, PyTypedField
+from sphinx.writers.html5 import HTML5Translator
+from sphinx.builders.html import StandaloneHTMLBuilder
+from sphinx.directives import ObjectDescription
 from docutils.parsers import rst
 
 import mock
+
 import ast
 import sys
 import executing
 
 DOMAIN = 'con'
+ALL = object()
+
+VERBOSE = False
+
+"""PROBLEMS: automodule doesn't give struct name,
+             autostruct gives name, must be placed first"""
 
 
 def Struct_mock__init__(self, *subcons, **subconskw):
@@ -25,6 +43,7 @@ def Struct_mock__init__(self, *subcons, **subconskw):
     It will allow a struct to remember it's name.
     The Struct will take on the name of the variable it's defined in.
     """
+
     # Regular things
     construct.core.Construct.__init__(self)
     self.subcons = list(subcons) + list(k/v for k, v in subconskw.items())
@@ -34,14 +53,15 @@ def Struct_mock__init__(self, *subcons, **subconskw):
 
     # Added things
     try:
-        node = executing.Source.executing(sys._getframe(2)).node
+        node = executing.Source.executing(sys._getframe(1)).node
         while hasattr(node, 'parent') and not isinstance(node, ast.Assign):
             node = node.parent
+            assert not isinstance(node, ast.Module)
         if isinstance(node.targets[0], ast.Name):
             self.name = node.targets[0].id
         elif isinstance(node.targets[0], ast.Attribute):
             self.name = node.targets[0].attr
-    except:
+    except Exception as e:
         node = None
 
 
@@ -105,9 +125,18 @@ class SubconDocumenter(ConstructDocumenter, ClassLevelDocumenter):
         s, sname, srefname, sdoc = deconstruct(self.object)
 
         if isinstance(s, construct.core.FormatField):
-            self.add_line('   :ctype: ' + s.fmtstr, sourcename)
+            self.add_line('   :fieldtype: ' + s.fmtstr, sourcename)
         elif isinstance(s, construct.core.Struct) and srefname:
-            self.add_line('   :reftype: ' + srefname, sourcename)
+            self.add_line('   :structtype: ' + srefname, sourcename)
+            link = ":con:struct:`" + srefname + '<' + srefname + '>`'
+
+    def generate(self, *args, **kwargs):
+        # generate makes rest formated output
+        super().generate(*args, **kwargs)
+        if VERBOSE:
+            print('-----result------')
+            for l in self.directive.result:
+                print(l)
 
 
 class StructDocumenter(ConstructDocumenter, ModuleLevelDocumenter):
@@ -132,14 +161,172 @@ class StructDocumenter(ConstructDocumenter, ModuleLevelDocumenter):
         return (False, [(sname, ssc) for ssc, sname, refname, sdoc in
                         [deconstruct(sc) for sc in s.subcons]])
 
+    def document_members(self, all_members=False):
+        # ModuleLevelDocumenter.document_members(self)
 
-class PyConstruct(PyVariable):
-    option_spec = PyVariable.option_spec.copy()
+        # set current namespace for finding members
+        self.env.temp_data['autodoc:module'] = self.modname
+        if self.objpath:
+            self.env.temp_data['autodoc:class'] = self.objpath[0]
+
+        want_all = all_members or self.options.inherited_members or \
+            self.options.members is ALL
+
+        # find out which members are documentable
+        members_check_module, members = self.get_object_members(want_all)
+
+        # document non-skipped members
+        memberdocumenters = []  # type: List[Tuple[Documenter, bool]]
+        for (mname, member, isattr) in self.filter_members(members, want_all):
+            classes = [cls for cls in self.documenters.values()
+                       if cls.can_document_member(member, mname, isattr, self)]
+            if not classes:
+                # don't know how to document this member
+                continue
+            # prefer the documenter with the highest priority
+            classes.sort(key=lambda cls: cls.priority)
+            # give explicitly separated module name, so that members
+            # of inner classes can be documented
+            full_mname = self.modname + '::' + \
+                '.'.join(self.objpath + [mname])
+            documenter = classes[-1](self.directive, full_mname, self.indent)
+            memberdocumenters.append((documenter, isattr))
+
+        for documenter, isattr in memberdocumenters:
+            documenter.generate(
+                all_members=True, real_modname=self.real_modname,
+                check_module=members_check_module and not isattr)
+
+        # reset current objects
+        self.env.temp_data['autodoc:module'] = None
+        self.env.temp_data['autodoc:class'] = None
+
+
+class desc_structref(nodes.Part, nodes.Inline, nodes.FixedTextElement):
+    pass
+
+
+class desc_pytype(nodes.Part, nodes.Inline, nodes.FixedTextElement):
+    pass
+
+
+class desc_ctype(nodes.Part, nodes.Inline, nodes.FixedTextElement):
+    pass
+
+
+class StructHTML5Translator(HTML5Translator):
+    def visit_type(self, node):
+        self.body.append(self.starttag(node, 'code', '',
+                                       CLASS='sig-prename descclassname'))
+        self.body.append('<span class="sig-paren">(</span>')
+
+    def depart_type(self, node):
+        self.body.append('<span class="sig-paren">)</span>')
+        self.body.append('</code>')
+
+    visit_desc_structref = visit_type
+    depart_desc_structref = depart_type
+    visit_desc_pytype = visit_type
+    depart_desc_pytype = depart_type
+
+    def visit_desc_ctype(self, node):
+        self.body.append(self.starttag(node, 'code', '',
+                                       CLASS='sig-prename descclassname',
+                                       STYLE='color: gray'))
+        self.body.append('<span>&ltparsed from ')
+
+    def depart_desc_ctype(self, node):
+        self.body.append('&gt</span>')
+        self.body.append('</code>')
+
+
+class StructStandaloneHTMLbuilder(StandaloneHTMLBuilder):
+    @property
+    def default_translator_class(self):
+        return StructHTML5Translator
+
+
+FF_TYPES = {
+    # FormatField : (pytype, ctype)
+    'e': ('float', '754 float'),
+    'f': ('float', 'float'),
+    'd': ('float', 'double'),
+    'b': ('int', 'signed char'),
+    'B': ('int', 'unsigned char'),
+    'h': ('int', 'short'),
+    'H': ('int', 'unsigned short'),
+    'L': ('int', 'unsigned long'),
+    'Q': ('int', 'integer'),
+    'l': ('int', 'long'),
+    'q': ('int', 'unsigned long long'),
+}
+
+
+def unformat(formatfieldstr):
+    return FF_TYPES[formatfieldstr[1]]
+
+
+class ConstructObjectDesc():
+    domain = DOMAIN
+
+    def add_target_and_index(self, name_cls, sig, signode):
+        modname = self.options.get(
+            'module', self.env.ref_context.get('py:module'))
+        fullname = name_cls[0]
+        node_id = make_id(self.env, self.state.document, '', fullname)
+
+        # note target
+        if fullname not in self.state.document.ids:
+            signode['names'].append(fullname)
+            signode['ids'].append(fullname)
+            signode['first'] = (not self.names)
+            self.state.document.note_explicit_target(signode)
+
+            domain = cast(PythonDomain, self.env.get_domain(DOMAIN))
+            domain.note_object(fullname, self.objtype, node_id)
+
+        indextext = self.get_index_text(modname, name_cls)
+        if indextext:
+            self.indexnode['entries'].append(('single', indextext,
+                                              fullname, '', None))
+
+
+# PyObject > ObjectDescription > SphinxDirective > Directive > object
+class Struct(ConstructObjectDesc, PyClasslike):
+    option_spec = PyClasslike.option_spec.copy()
+
+
+# PyObject > ObjectDescription > SphinxDirective > Directive > object
+class Subcon(ConstructObjectDesc, PyAttribute):
+    option_spec = PyAttribute.option_spec.copy()
     option_spec.update({
-        'ctype': rst.directives.unchanged,
-        'reftype': rst.directives.unchanged,
+        'structtype': rst.directives.unchanged,
+        'fieldtype': rst.directives.unchanged,
         'default_value': rst.directives.unchanged
     })
+
+    def handle_signature(self, sig, signode):
+        """ Almost there, this is for sure where the money happens.
+         create new nodes and you'll get there for sure
+        """
+        fullname, prefix = super().handle_signature(sig, signode)
+        structtype = self.options.get('structtype')
+        if structtype:
+            refnode = addnodes.pending_xref('', refdomain=DOMAIN, refexplicit=False,
+                                            reftype='struct', reftarget=structtype)
+            refnode += desc_structref(structtype, structtype)
+            signode += refnode
+        fieldtype = self.options.get('fieldtype')
+        if fieldtype:
+            pytype, ctype = unformat(fieldtype)
+
+            pyref = addnodes.pending_xref('', refdomain='py', refexplicit=False,
+                                          reftype='class', reftarget=pytype)
+            pyref += desc_pytype(pytype, pytype)
+            signode += pyref
+            signode += desc_ctype(ctype, ctype)
+
+        return fullname, prefix
 
 
 class ConstructPythonDomain(PythonDomain):
@@ -148,17 +335,18 @@ class ConstructPythonDomain(PythonDomain):
 
     object_types = {'struct': ObjType(_('data'), 'data', 'obj'),
                     'subcon': ObjType(_('data'), 'data', 'obj')}
-    directives = {'struct': PyConstruct,
-                  'subcon': PyConstruct}
+    directives = {'struct': Struct,
+                  'subcon': Subcon}
     roles = {'struct': PyXRefRole(),
              'subcon': PyXRefRole()}
 
-    def __init__(self, env):
-        super().__init__(env)
-
 
 def setup(app):
+    app.add_builder(StructStandaloneHTMLbuilder, override=True)
     app.add_domain(ConstructPythonDomain)
+    app.add_node(desc_structref)
+    app.add_node(desc_pytype)
+    app.add_node(desc_ctype)
     app.add_autodocumenter(StructDocumenter)
     app.add_autodocumenter(SubconDocumenter)
 
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,10 +42,13 @@ release = u'0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
+sys.path.append(os.path.abspath('../ext'))
 sys.path.append(os.path.abspath('../../'))
+
 extensions = [
     'sphinx.ext.autodoc',
-    'docs.ext.autoconstruct',
+    'autoconstruct',
     'sphinx.ext.intersphinx',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
