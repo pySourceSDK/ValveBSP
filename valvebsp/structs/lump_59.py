59800b74b38624193576d35f94372b5bc5f2b02e
maxdup <mdupuis@hotmail.ca>
2021-05-19 17:06:30 -0400

    improved lump_59 parsing

--- valvebsp/structs/lump_59.py
+++ valvebsp/structs/lump_59.py
@@ -15,7 +15,12 @@ standard_library.install_aliases()
 from construct import *  # NOQA: #402
 from valvebsp.structs.common import *  # NOQA #402
 
-flags_t = Struct('levelFlags' / Int32ul)
+flags_flags = FlagsEnum(
+    Int32ul,
+    BAKED_STATIC_PROP_LIGHTING_NONHDR=1,
+    BAKED_STATIC_PROP_LIGHTING_HDR=2)
+
+flags_t = Struct('levelFlags' / flags_flags)
 
 
 @lump_struct
