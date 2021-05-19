9aa426f6ac09800d26427d1ce09907146eb573be
maxdup <mdupuis@hotmail.ca>
2021-05-19 16:56:13 -0400

    improved lump 6 parsing

--- valvebsp/structs/lump_6.py
+++ valvebsp/structs/lump_6.py
@@ -15,10 +15,27 @@ standard_library.install_aliases()
 from construct import *  # NOQA: #402
 from valvebsp.structs.common import *  # NOQA: #402
 
+texinfo_flags8 = FlagsEnum(Int32ul,
+                           SURF_LIGHT=1,
+                           SURF_SKY2D=2,
+                           SURF_SKY=4,
+                           SURF_WARP=8,
+                           SURF_TRANS=16,
+                           SURF_NOPORTAL=32,
+                           SURF_TRIGGER=64,
+                           SURF_NODRAW=128,
+                           SURF_HINT=256,
+                           SURF_SKIP=512,
+                           SURF_NOLIGHT=1024,
+                           SURF_BUMPLIGHT=2048,
+                           SURF_NOSHADOWS=4096,
+                           SURF_NODECALS=8192,
+                           SURF_NOCHOP=16384,
+                           SURF_HITBOX=32768)
 texinfo_t = Struct(
     'textureVecsTexelsPerWorldUnits' / Float32l[2][4],
     'lightmapVecsLuxelsPerWorldUnits' / Float32l[2][4],
-    'flags' / Int32sl,
+    'flags' / texinfo_flags8,
     'texData' / Int32sl * 'index into :ref:`lump 2<lump_2>`',
 )
 
