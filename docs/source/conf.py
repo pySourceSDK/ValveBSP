b5388088a87c9a6ecdb3526c029124176aaa9393
maxdup <mdupuis@hotmail.ca>
2023-07-28 20:08:47 -0400

    added workflow for automated doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,11 +43,8 @@ release = u'1.1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 
-sys.path.append(os.path.abspath('../../../sphinx-autodoc-construct'))
-sys.path.append(os.path.abspath('../../'))
-
 extensions = [
-    'sphinx-autodoc-construct',
+    'sphinx_autodoc_construct',
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.ifconfig',
